8445cc837623f46adaa1628fbdf1d3df59dada56
Hugo Naturil <naturilhugo8@gmail.com>
2025-10-27 18:18:28 +0000

    Pon alguna mas por abajo y las dos de arriba del todo que no se vean tan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,8 +81,8 @@ export default function Home() {
 
         {/* How It Works Section */}
         <section id="how-it-works" className="py-20 bg-card overflow-hidden relative">
-          <div className="absolute top-0 left-0 w-72 h-72 bg-blue-300/50 rounded-full mix-blend-multiply filter blur-xl opacity-75 animate-blob"></div>
-          <div className="absolute top-0 right-0 w-72 h-72 bg-pink-300/50 rounded-full mix-blend-multiply filter blur-xl opacity-75 animate-blob animation-delay-2000"></div>
+          <div className="absolute top-0 left-0 w-72 h-72 bg-blue-300/50 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob"></div>
+          <div className="absolute top-0 right-0 w-72 h-72 bg-pink-300/50 rounded-full mix-blend-multiply filter blur-xl opacity-50 animate-blob animation-delay-2000"></div>
           <div className="absolute bottom-1/4 left-1/4 w-72 h-72 bg-yellow-300/30 rounded-full mix-blend-multiply filter blur-2xl opacity-75 animate-blob animation-delay-4000"></div>
           <div className="absolute bottom-0 right-1/4 w-72 h-72 bg-purple-300/30 rounded-full mix-blend-multiply filter blur-2xl opacity-75 animate-blob animation-delay-1000"></div>
           
