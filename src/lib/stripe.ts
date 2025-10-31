4fa2195a0b03e889ffc43fca9f046e4db18d4779
Hugo Naturil <naturilhugo8@gmail.com>
2025-10-31 19:05:07 +0000

    Actualmente, el frontend está intentando usar la función stripe.redirect

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,7 +2,6 @@
 
 import { addDoc, onSnapshot, Firestore } from 'firebase/firestore';
 import { customerCheckoutSessionsCollectionRef } from '@/firebase/firestore/references';
-import { loadStripe } from '@stripe/stripe-js';
 
 export async function createCheckoutSession(
   db: Firestore,
@@ -14,31 +13,27 @@ export async function createCheckoutSession(
   // 1. Create a new checkout session document in Firestore.
   const docRef = await addDoc(checkoutSessionsRef, {
     price: priceId,
+    mode: 'subscription', // Specify subscription mode
     success_url: window.location.origin + '/perfil',
     cancel_url: window.location.origin + '/precios',
   });
 
-  // 2. Wait for the CheckoutSession to get a session ID from the Stripe extension.
-  onSnapshot(docRef, async (snap) => {
-    const { error, sessionId } = snap.data() || {};
+  // 2. Wait for the CheckoutSession to get a URL from the Stripe extension.
+  onSnapshot(docRef, (snap) => {
+    const data = snap.data();
+    if (data) {
+      const { error, url } = data;
 
-    if (error) {
-      // Show an error to your customer and inspect your Cloud Function logs in the Firebase console.
-      console.error(`An error occurred: ${error.message}`);
-      alert(`An error occurred: ${error.message}`); // Consider using a toast notification
-      return;
-    }
-
-    if (sessionId) {
-      // We have a session, let's redirect to Checkout.
-      const publishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
-      if (!publishableKey) {
-        throw new Error('Stripe publishable key not found.');
+      if (error) {
+        // Show an error to your customer and inspect your Cloud Function logs in the Firebase console.
+        console.error(`An error occurred: ${error.message}`);
+        alert(`An error occurred: ${error.message}`); // Consider using a toast notification
+        return;
       }
 
-      const stripe = await loadStripe(publishableKey);
-      if (stripe) {
-        await stripe.redirectToCheckout({ sessionId });
+      if (url) {
+        // We have a URL, let's redirect to Checkout.
+        window.location.assign(url);
       }
     }
   });
