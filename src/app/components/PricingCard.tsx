8ebbd837499c5c7b189fd2772710fc250b6e1c22
Hugo Naturil <naturilhugo8@gmail.com>
2025-11-29 09:21:11 +0000

    No has añadido nada de lo que te he dicho

--- src/app/components/PricingCard.tsx
+++ src/app/components/PricingCard.tsx
@@ -8,10 +8,7 @@ import { useMessages, useTranslations } from "next-intl";
 
 type PricingPlan = {
   id:string;
-  // name: string;
   price: string;
-  // credits: string;
-  // features: string[];
   stripePriceId: string;
 };
 
@@ -52,6 +49,7 @@ export default function PricingCard({ plan, onCtaClick, isLoading, isCurrentUser
             </li>
           ))}
         </ul>
+        <p className="font-bold text-foreground mt-4">{t(`${plan.id}.storyCount`)}</p>
       </CardContent>
       <CardFooter>
         <Button 
