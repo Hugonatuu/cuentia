492be47c6dc5d5dbb7bb44c7159f9aff30571a92
Hugo Naturil <naturilhugo8@gmail.com>
2025-11-29 09:59:55 +0000

    Quiero que hagas el boton de mas popular mas largo y las letras un poco

--- src/app/[locale]/precios/page.tsx
+++ src/app/[locale]/precios/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState } from 'react';
@@ -216,9 +217,9 @@ export default function PreciosPage() {
                       <div key={t(`pricingPlans.${plan.id}.name`)} className="flex flex-col relative">
                         {plan.id === 'special' && (
                            <div className="absolute -top-4 left-1/2 -translate-x-1/2 z-10">
-                            <div className="bg-gradient-to-r from-blue-400 to-purple-500 text-white px-4 py-1.5 rounded-full flex items-center gap-2 shadow-lg">
+                            <div className="bg-gradient-to-r from-blue-400 to-purple-500 text-white px-6 py-1 rounded-full flex items-center gap-2 shadow-lg">
                                 <Gem className="h-5 w-5" />
-                                <span className="text-sm font-bold tracking-wider">
+                                <span className="text-xs font-bold tracking-wider">
                                 {t('mostPopularBadge')}
                                 </span>
                             </div>
@@ -242,3 +243,4 @@ export default function PreciosPage() {
     </div>
   );
 }
+
