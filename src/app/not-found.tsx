64c41202d3f1184007722201bed478fe56d8936f
Hugo Naturil <naturilhugo8@gmail.com>
2026-02-10 08:52:57 +0000

    en la pagina de favicon he puesto el logo de la app

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import Error from 'next/error';
 
 // This page renders when a route like `/unknown.txt` is requested.
 // In this case, the layout at `app/[locale]/layout.tsx` receives
-// an invalid value as the `[locale]` param and calls `notFound()`.
+// an invalid value as the `[locale]` param and calls `notFound(
 
 export default function GlobalNotFound() {
   return (
